8bdd001bfbc53d349f234d04b3c6736ef884635b
Mohammad Nazmul <141314437+nazmull139@users.noreply.github.com>
2025-06-29 23:47:23 +0600

    Update Login.jsx

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -20,7 +20,7 @@ const Login = () => {
     try { 
 
       if (state === 'Sign Up') {
-        const {data} = await axios.post(backendUrl + '/api/user/register', {name,email,password})  
+       /* const {data} = await axios.post(backendUrl + '/api/user/register', {name,email,password})  
         //console.log(data)
         if (data.success) {
           localStorage.setItem('token', data.token)
@@ -29,7 +29,7 @@ const Login = () => {
            
         } else {
           toast.error(data.message)
-      }
+      }*/
 
     } else {
       const {data} = await axios.post(backendUrl + '/api/user/login', {email,password}) 
