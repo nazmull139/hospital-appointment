d97e89d17e15d351cf39f92964b56eccd832f501
Mohammad Nazmul <141314437+nazmull139@users.noreply.github.com>
2025-07-24 18:28:39 +0600

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -14,8 +14,8 @@ const Contact = () => {
 
           <div className='flex flex-col justify-center items-start gap-6'>
               <p className='font-semibold text-lg text-gray-600'>Our Office</p>
-              <p className='text-gray-500'>54709 Willms Station <br/> Suite 350, Washington, USA</p>
-              <p className='text-gray-500'>Tel: (415) 555‑0132 <br/> Email: greatstackdev@gmail.com</p>
+              <p className='text-gray-500'>Hazaribagh Dhaka , 1209</p>
+              <p className='text-gray-500'>phone : +880 1705776033<br/> Email: nazmul210121@diit.edu.bd</p>
               <p className='font-semibold text-lg text-hray-600'>Careers at PRESCRIPTO </p>
               <p className='text-gray-500'>Learn more about our teams and job openings.</p>
               <button className='border border-black px-8 py-4 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
