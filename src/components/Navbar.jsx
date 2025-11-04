1de087cc058f60e77d08fb223261881664b40d4b
Mohammad Nazmul <141314437+nazmull139@users.noreply.github.com>
2025-11-04 13:39:01 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -61,7 +61,7 @@ const Navbar = () => {
             {/** Mobile menu */}
             <div className={`${showMenu ? 'fixed w-full':'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`} >
                 <div className='flex items-center justify-between px-5 py-6'>
-                    <img className='w-36' src={assets.logo} />
+                    <img className='w-36' src={assets.diit} />
                     <img className='w-7' onClick={()=> setShowMenu(false)} src={assets.cross_icon} />
                 </div>
                 <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium'>
